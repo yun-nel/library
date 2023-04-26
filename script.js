b23f19844fa5d28c5f43edbc3a60cbfd8cc38f4f
jaeyun <d65971@gmail.com>
2023-04-26 17:35:48 +0930

    add logic for opening and closing modal

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function Book(title, author, pages, read) {
 const myLibrary = [];
 
 function addBookToLibrary() {
-  const book = new Book('One Piece', 'Eiichiro Oda', '21,450', 'Read');
+  const book = new Book('One Piece', 'Eiichiro Oda', '21,450', 'Completed');
   myLibrary.push(book);
 }
 
@@ -34,3 +34,27 @@ addBookToLibrary();
 addBookToLibrary();
 console.log(myLibrary);
 displayBook();
+
+// Modal logic
+const newBookBtn = document.querySelector('.new-book');
+const submitBook = document.querySelector('.submit-book');
+const modal = document.querySelector('.modal');
+const body = document.querySelector('body');
+
+// Open
+const darkBg = document.createElement('div');
+
+newBookBtn.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+  darkBg.classList.add('dark-bg');
+  body.insertAdjacentElement('afterbegin', darkBg);
+});
+
+// Close
+function closeModal() {
+  modal.classList.remove('show-modal');
+  darkBg.remove();
+}
+
+submitBook.addEventListener('click', () => closeModal());
+darkBg.addEventListener('click', () => closeModal());
