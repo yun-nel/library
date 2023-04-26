dd5c9b1ab2fff2cc6cc37e33081e5ab0ed704a9c
jaeyun <d65971@gmail.com>
2023-04-26 14:22:49 +0930

    add displayBook function
    This commit adds a new function displayBook that loops over the myLibrary array and creates a new card element for each book. It also adds each property of the book object as a paragraph element to the card using insertAdjacentElement(). The card element is then appended to the DOM. This function makes it easier to render the books in the library and display them on the page.

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ function addBookToLibrary() {
   const book = new Book('One Piece', 'Eiichiro Oda', '21,450', 'Read');
   myLibrary.push(book);
 }
+
+function displayBook() {
+  const container = document.getElementById('book-container');
+
+  myLibrary.forEach((book) => {
+    const card = document.createElement('div');
+    card.classList.add('book');
+    const bookKeys = Object.keys(book);
+
+    bookKeys.forEach((key) => {
+      const p = document.createElement('p');
+      p.textContent = book[key];
+      card.insertAdjacentElement('beforeend', p);
+    });
+    container.appendChild(card);
+  });
+}
+
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+console.log(myLibrary);
+displayBook();
